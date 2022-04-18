7420a1a8195d0fac505868e20a85f4293ba8d503
Marcin Bernecki <71261155+MarcinBerry@users.noreply.github.com>
2022-04-18 16:18:17 +0200

    initial commit

---
+++ ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  Projekt
+//
+//  Created by student on 10/04/2022.
+//  Copyright © 2022 student. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+           NavigationView{
+            VStack{
+                HStack{
+                Rectangle().fill(Color.red).frame(width: 40, height: 40)
+                Spacer()
+                NavigationLink(destination: SubPage(img: "BMW",marka: "BMW", pojemonsc: "3.0", typ: "sedan", rokProdukcji: "2007"), label: {
+                    Text("Oferta").frame(width: 60, height: 50)
+                })
+                }
+                VStack(alignment: .center){
+                    NavigationLink(destination: SubPage(img: "BMW",marka: "BMW", pojemonsc: "3.0", typ: "sedan", rokProdukcji: "2007"), label: {
+                        Text("BMW").frame(width: 375, height: 50).background(Color.gray)
+                    })
+                    NavigationLink(destination: SubPage(marka: "Audi", pojemonsc: "2.0", typ: "kombi", rokProdukcji: "2010"), label: {
+                        Text("Audi").frame(width: 375, height: 50).background(Color.green)
+                    })
+                    NavigationLink(destination: SubPage(marka: "Mercedes", pojemonsc: "1.0", typ: "cabrio", rokProdukcji: "2004"), label: {
+                        Text("Mercedes").frame(width: 375, height: 50).background(Color.yellow)
+                    })
+                    Spacer()
+                }
+            }.padding(10)
+        }
+        }
+}
+
+struct SubPage: View {
+    var img: String?
+    var marka: String
+    var pojemonsc: String
+    var typ: String
+    var rokProdukcji: String
+    var body: some View {
+            VStack {
+                (img != nil) ? Image(img!).resizable()
+                .aspectRatio(contentMode: .fit) : nil
+                Text("Marka: \(marka)")
+                Text("Pojemnosc: \(pojemonsc)")
+                Text("Typ: \(typ)")
+                Text("Rok produkcji: \(rokProdukcji)")
+            }
+    }
+}
