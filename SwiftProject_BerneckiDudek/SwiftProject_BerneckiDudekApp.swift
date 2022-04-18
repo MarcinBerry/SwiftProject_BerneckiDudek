a3fef8b5d82034d2fae566842f378c81e590cc0a
BerneckiDudek <marcinbernecki.priv@gmail.com>
2022-04-18 07:52:02 -0700

    Initial Commit

---
+++ SwiftProject_BerneckiDudek/SwiftProject_BerneckiDudekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftProject_BerneckiDudekApp.swift
+//  SwiftProject_BerneckiDudek
+//
+//  Created by Marcin on 18/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftProject_BerneckiDudekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
