8723fed804c913bfc371084ab55e4acb5afa1e3c
Dudek <dudek@Dudeks-Mac.local>
2022-04-19 19:37:43 +0200

    Mozliwosc dodania

---
+++ SwiftProject_BerneckiDudek/AddItemAlert.swift
@@ -0,0 +1,43 @@
+//
+//  AddItemAlert.swift
+//  SwiftProject_BerneckiDudek
+//
+//  Created by Dudek on 19/04/2022.
+//
+
+import Foundation
+import SwiftUI
+//
+//struct AddItem: View {
+//
+//    let screenSize = UIScreen.main.bounds
+//
+//    @State var addCar = false
+//    @State var addCarName: String = ""
+//    @State var addCarPojemnosc: String = ""
+//    @State var addCarNadwozie: String = ""
+//    @State var addCarRocznik: String = ""
+//    var body: some View {
+//        VStack {
+//            Text("Dodaj auto")
+//            TextField("Podaj marke auta:", text: self.$addCarName).textFieldStyle(RoundedBorderTextFieldStyle())
+//            TextField("Podaj pojemnosc auta:", text: self.$addCarPojemnosc).textFieldStyle(RoundedBorderTextFieldStyle())
+//            TextField("Podaj nadwozie auta:", text: self.$addCarNadwozie).textFieldStyle(RoundedBorderTextFieldStyle())
+//            TextField("Podaj rocznik auta:", text: self.$addCarRocznik).textFieldStyle(RoundedBorderTextFieldStyle())
+//
+//            Button(action: {
+//                self.Car.append(Car(Nazwa: addCarName, Pojemnosc: addCarPojemnosc, nadwozie: addCarNadwozie, dataprodukcji: addCarRocznik))
+//
+//                self.addCar.toggle()
+//
+//                self.addCarName = ""
+//                self.addCarRocznik = ""
+//                self.addCarNadwozie = ""
+//                self.addCarPojemnosc = ""
+//            })
+//        }  .padding()
+//            .frame(width: screenSize.width * 0.7, height: screenSize.height * 0.6)
+//            .background(Color.gray)
+//            .clipShape(RoundedRectangle(cornerRadius: 20.0, style: .continuous))
+//    }
+//}
