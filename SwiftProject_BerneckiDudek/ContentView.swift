e80b631802b18ed5277480e3581fbda2c0bb299c
BerneckiDudek <marcinbernecki.priv@gmail.com>
2022-04-19 09:50:46 -0700

    Naprawienie Navigacji

--- SwiftProject_BerneckiDudek/ContentView.swift
+++ SwiftProject_BerneckiDudek/ContentView.swift
@@ -12,29 +12,17 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             VStack(alignment: .center) {
-                NavComponent()
                 Spacer()
                 Text("Komis samochodowy Kacpero i Marcino")
                 Text("ul. Kaczadafiego 96,")
                 Text("21-010 Warszawa")
                 Spacer()
             }
-            
-            //                VStack(alignment: .center){
-            //                    NavigationLink(destination: SubPage(img: "BMW",marka: "BMW", pojemonsc: "3.0", typ: "sedan", rokProdukcji: "2007"), label: {
-            //                        Text("BMW").frame(width: 375, height: 50).background(Color.gray)
-            //                    })
-            //                    NavigationLink(destination: SubPage(marka: "Audi", pojemonsc: "2.0", typ: "kombi", rokProdukcji: "2010"), label: {
-            //                        Text("Audi").frame(width: 375, height: 50).background(Color.green)
-            //                    })
-            //                    NavigationLink(destination: SubPage(marka: "Mercedes", pojemonsc: "1.0", typ: "cabrio", rokProdukcji: "2004"), label: {
-            //                        Text("Mercedes").frame(width: 375, height: 50).background(Color.yellow)
-            //                    })
-            //                    Spacer()
-            //                }
+            .navigationTitle("Kacpero&Marcino")
+            .navigationBarItems(trailing: NavigationLink(destination: CarsView(), label: {
+                Text("Oferta").frame(width: 60, height: 50)
+            }))
         }
-        
-        
     }
 }
 
