834946e5517ca802828e9593bcbb2aac9377ff44
Dudek <dudek@Dudeks-Mac.local>
2022-04-19 16:59:58 +0200

    Lista, usuwanie i wyswietlanie

--- SwiftProject_BerneckiDudek/NavComponent.swift
+++ SwiftProject_BerneckiDudek/NavComponent.swift
@@ -13,7 +13,10 @@ struct NavComponent: View {
         HStack{
         Rectangle().fill(Color.red).frame(width: 40, height: 40)
         Spacer()
-        NavigationLink(destination: SubPage(img: "BMW",marka: "BMW", pojemonsc: "3.0", typ: "sedan", rokProdukcji: "2007"), label: {
+//        NavigationLink(destination: SubPage(img: "BMW",marka: "BMW", pojemonsc: "3.0", typ: "sedan", rokProdukcji: "2007"), label: {
+//            Text("Oferta").frame(width: 60, height: 50)
+//        })
+        NavigationLink(destination: CarsView(), label: {
             Text("Oferta").frame(width: 60, height: 50)
         })
         }
